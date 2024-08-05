70933e07c5fcef6a9dac0693f70eb764e7a1d1f5
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-08-05 13:05:35 +0900

    boj_10845

---
+++ cpp/boj_10845.cpp
@@ -0,0 +1,43 @@
+// https://www.acmicpc.net/problem/10845
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    queue<int> q;
+    
+    int N;
+    cin >> N;
+    
+    while (N--) {
+        string command;
+        cin >> command;
+        
+        if (command == "push") {
+            int data;
+            cin >> data;
+            q.push(data);
+        } else if (command == "pop") {
+            if (!q.empty()) {
+                cout << q.front() << '\n';
+                q.pop();
+            } else {
+                cout << -1 << '\n';
+            }
+        } else if (command == "size") {
+            cout << q.size() << '\n';
+        } else if (command == "empty") {
+            cout << q.empty() << '\n';
+        } else if (command == "front") {
+            cout << (q.empty() ? -1 : q.front()) << '\n';
+        } else if (command == "back") {
+            cout << (q.empty() ? -1 : q.back()) << '\n';
+        } 
+    }
+    return 0;
+}
