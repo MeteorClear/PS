59f9fd90a76a59652bc8191ceec886ec04c388e1
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-08-27 16:36:33 +0900

    boj_15651

---
+++ boj/cpp/boj_15651.cpp
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/15651
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void permutation(vector<int>& p, int n, int m) {
+    if (p.size() == m) {
+        for (auto e : p) {
+            cout << e << ' ';
+        } cout << '\n';
+        return;
+    }
+    
+    for (int i=1; i<=n; ++i) {
+        p.push_back(i);
+        permutation(p, n, m);
+        p.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> p;
+    
+    permutation(p, N, M);
+    
+    return 0;
+}
