3d05dfd0dff245dd7228e8db3281e9f704a6789f
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-08-28 14:46:15 +0900

    boj_15654

---
+++ boj/cpp/boj_15654.cpp
@@ -0,0 +1,43 @@
+https://www.acmicpc.net/problem/15654
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void makePermutaion(vector<int>& v, vector<int>& p, int m) {
+    if (p.size() == m) {
+        for (auto e : p) {
+            cout << e << ' ';
+        } cout << '\n';
+        return;
+    }
+    
+    for (int i=0; i<v.size(); ++i) {
+        if (find(p.begin(), p.end(), v[i]) != p.end()) continue;
+        
+        p.push_back(v[i]);
+        makePermutaion(v, p, m);
+        p.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> v(N), p;
+    
+    for (int i=0; i<N; ++i) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    
+    makePermutaion(v, p, M);
+
+    return 0;
+}
