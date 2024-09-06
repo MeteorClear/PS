068fb8455c212d8bdd5f08bec5d9298940350841
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-09-06 16:10:49 +0900

    boj_10815

---
+++ boj/cpp/boj_10815.cpp
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/10815
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    vector<int> A(N);
+    for (int i=0; i<N; ++i) cin >> A[i];
+    
+    sort(A.begin(), A.end());
+    
+    int M;
+    cin >> M;
+    
+    while (M--) {
+        int target;
+        cin >> target;
+        
+        bool res = binary_search(A.begin(), A.end(), target);
+        cout << res << ' ';
+    }
+
+    return 0;
+}
