2d464afec7ff5db9522a40dd00f16917aea95e3f
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2025-01-14 22:39:27 +0900

    boj_20920

--- boj/cpp/boj_20920.cpp
+++ boj/cpp/boj_20920.cpp
@@ -8,41 +8,34 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	int N, M;
-	cin >> N >> M;
-	
-	unordered_map<string, int> vocabulary;
-	
-	while (N--) {
-	    string word;
-	    cin >> word;
-	    
-	    if (word.size() < M) continue;
-	    
-	    if (vocabulary.find(word) == vocabulary.end()) {
-	        vocabulary[word] = 1;
-	    } else {
-	        ++vocabulary[word];
-	    }
-	}
-	
-	vector<pair<string, int>> note;
-	for (auto [word, count] : vocabulary) note.push_back({word, count});
-	
-	sort(note.begin(), note.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
-	    if (a.second == b.second) {
-	        if (a.first.size() == b.first.size()) {
-	            return a.first < b.first;
-	        }
-	        return a.first.size() > b.first.size();
-	    }
-	    return a.second > b.second;
-	});
-	
-	for (auto [word, count] : note) cout << word << '\n';
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    unordered_map<string, int> vocabulary;
+    vocabulary.reserve(N);
+    
+    while (N--) {
+        string word;
+        cin >> word;
+        
+        if (word.size() < M) continue;
+        ++vocabulary[word];
+    }
+    
+    vector<pair<string, int>> note;
+    note.reserve(vocabulary.size());
+    for (auto [word, count] : vocabulary) note.emplace_back(word, count);
+    
+    sort(note.begin(), note.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+        if (a.second != b.second) return a.second > b.second;
+        if (a.first.size() != b.first.size()) return a.first.size() > b.first.size();
+        return a.first < b.first;
+    });
+    
+    for (auto [word, count] : note) cout << word << '\n';
+    
     return 0;
 }
