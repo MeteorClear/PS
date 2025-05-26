9578f60538fc5e0152bdfcb5b5e95240056c15c4
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2025-05-26 17:01:56 +0900

    boj_9659

---
+++ boj/cpp/boj_9659.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/9659
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long N;
+    cin >> N;
+    cout << (N&1 ? "SK" : "CY");
+
+    return 0;
+}
