86109dcf58de38c1b8c673b15fe167eeae9fdad4
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2025-07-11 18:08:58 +0900

    [11399] ATM

--- boj/cpp/boj_11399.cpp
+++ boj/cpp/boj_11399.cpp
@@ -1,4 +1,8 @@
 // https://www.acmicpc.net/problem/11399
+// 11399번 ATM
+// 정렬, 그리디 문제
+// 인출 시간을 오름차순 정렬한 후 누적 합을 구해 총합을 구해 해결 가능
+// 각 사람이 돈을 인출하는데 필요한 시간의 합은 인출 시간이 짧은 사람을 앞에 배치할수록 줄어듬
 
 #include <iostream>
 #include <vector>
